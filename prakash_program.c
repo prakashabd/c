fdea6100c5b8b6226cb5add7e52363b88d7ca0da
prakashabd <prakash2005abd@gmail.com>
2025-05-05 13:30:55 +0530

    Create prakash_program.c

---
+++ prakash_program.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+//PRAKASH NEW LEARNING
+int main(){
+    int a,b;
+    printf("enter the two numbers:");
+    scanf("%d %d",&a, &b);
+    printf("%d",a+b);
+
+    return 0;
+    
+}
