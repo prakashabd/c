1c31f4959971fe508efb6aa379b3a8420242b297
prakashabd <prakash2005abd@gmail.com>
2025-05-05 13:34:37 +0530

    Create arithmatic_program.c

---
+++ arithmatic_program.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+//PRAKASH NEW LEARNING
+int main(){
+    int a,b,sum,div, sub,mul;
+    printf("enter the two numbers:");
+    scanf("%d %d",&a, &b);
+    sum= a+b;
+    sub = a - b;
+    mul = a*b;
+    div = a/b;
+    printf("sum = %d\n",sum);
+    printf("sub = %d\n",sub);
+    printf("mul = %d\n",mul);
+    printf("div = %d\n",div);
+    
+    
+
+    return 0;
+    
+}
